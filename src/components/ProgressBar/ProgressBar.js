8a2b9c7c900e3126b1385cb768f79303b9158861
Mohammed Abdallah <mohammedahelmi@gmail.com>
2026-04-18 08:44:56 +0200

    Implement progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,53 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  value = Math.max(0, Math.min(100, value));
+  const sizeConfig = {
+    large: {
+      '--padding': '4px',
+      '--outer-border-radius': '8px',
+      '--height': '24px'
+    },
+    medium: {
+      '--outer-border-radius': '4px',
+      '--height': '16px'
+    },
+    small: {
+      '--outer-border-radius': '4px',
+      '--height': '8px'
+    }
+  };
+  return (
+    <Wrapper role="progressbar" aria-valuenow={value} style={sizeConfig[size]}>
+      <BarWrapper>
+        <Bar style={{ '--percentage': `${value}%` }}>
+          <VisuallyHidden>{value}%</VisuallyHidden>
+        </Bar>
+      </BarWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: var(--padding);
+  border-radius: var(--outer-border-radius);
+  height: var(--height);
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`
+const BarWrapper = styled.div`
+  height: 100%; 
+  width: 100%; 
+  background-color: 'transparent';
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  height: 100%;
+  width: var(--percentage); 
+  background-color: ${COLORS.primary};
+`
 
 export default ProgressBar;
